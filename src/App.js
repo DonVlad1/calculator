0eb82c37d7cfd6ca523e3367dd945b0e72245d68
Vlad <vladut_stoian@live.co.uk>
2022-08-10 12:59:43 +0100

    Made a very simple .map using lecture notes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// import the useState hook if you want to store values in the state
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => 
+{
+  const [calculatorButtons, setCalculatorButtons] = useState([0,1,2,3,4,5,6,7,8,9,"+","-","="])
 
-export default App;
+  // state = {
+  //   calculatorButtons: [0,1,2,3,4,5,6,7,8,9,"+","-","="]
+
+  return (
+    <div>
+      {calculatorButtons.map((button, buttonKey) =>
+      {
+        return <h1 key = { buttonKey }>The button is {button}</h1>
+      })}
+    </div>
+  )
+}
+export default App
+
+
+
+
+    //   <div>
+    //     <h2>If/else statement</h2>
+    //     <p>Number is {add}</p>
+    //     {/* <button onClick={() => setToggle(!toggle)}>Toggle</button> */}
+    //     <button onClick={function() {(addNumber(calculatorButtons[0] += add))}}>0</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons[1] += add))}}>1</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons[2] += add))}}>2</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons[3] += add))}}>3</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons += add))}}>4</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons += add))}}>5</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons += add))}}>6</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons += add))}}>7</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons += add))}}>8</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons += add))}}>9</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons += add))}}>+</button>
+    //     <button onClick={function() {(addNumber(calculatorButtons += add))}}>-</button>
+    //   </div>
+    // )
+    // else
+    // {
+    //   <h2>Fail</h2>
+    //   gameState = false
+    // }
