31937c99e1dc9ebf6a1fcf3875c1743740f8572b
Vlad <vladut_stoian@live.co.uk>
2022-08-11 15:50:01 +0100

    Made it so it checks for last character. Does nothing if it find either / or * twice

--- src/App.js
+++ src/App.js
@@ -4,36 +4,40 @@ import { evaluate } from 'mathjs'
 
 const App = () => 
 {
-
-const calculatorButtons = [0,1,'+','-','C', 'Del','=']
+const calculatorButtons = [0,1,2,3,4,5,6,7,8,9,'+','-','/','*','C','Del','=']
 const [selectedButtons, setButtonSelection] = useState([])
 const [calculatorTotal, setCalculatorTotal] = useState([])
 
   return (
     <div>
-      <h1>Numbers are {selectedButtons}</h1>
-      <h1>Calculator Total is {calculatorTotal}</h1>
+      <div className="calculatorTotalScreen">
+        <h1>Numbers are {selectedButtons}</h1>
+        <h1>Calculator Total is {calculatorTotal}</h1>
+      </div>
+      
+      <div>
       {calculatorButtons.map((button, buttonKey) =>
-      {
-        
-        if (button === '=') 
         {
-          return(<button key= { buttonKey } onClick={() => setCalculatorTotal(returnResult(selectedButtons))}>=</button>)
-        }
-        else if (button === 'C')
-        {
-          return(<button key = { buttonKey } onClick={() => clearAll(setButtonSelection, setCalculatorTotal)}>C</button>)
-        }
-        else if (button === 'Del')
-        {
-          console.log(selectedButtons)
-          return(<button key = { buttonKey } onClick={() => setButtonSelection(deleteLast(selectedButtons))}>Del</button>)
-        }
-        else
-        {
-          return (<button key = { buttonKey } onClick={() => setButtonSelection(addButtons(selectedButtons, button))}>{button}</button>)
-        }
-      })}
+          console.log(selectedButtons[selectedButtons.length - 1]) 
+          if (button === '=') 
+          {
+            return(<button key= { buttonKey } onClick={() => setCalculatorTotal(returnResult(selectedButtons))}>=</button>)
+          }
+          else if (button === 'C')
+          {
+            return(<button key = { buttonKey } onClick={() => clearAll(setButtonSelection, setCalculatorTotal)}>C</button>)
+          }
+          else if (button === 'Del')
+          {
+            return(<button key = { buttonKey } onClick={() => setButtonSelection(deleteLast(selectedButtons))}>Del</button>)
+          }
+          else
+          {
+            return (<button key = { buttonKey } onClick={() => setButtonSelection(addButtons(selectedButtons, button))}>{button}</button>)
+          }
+        })}
+      </div>
+
     </div>
   )
 }
@@ -50,12 +54,20 @@ function returnResult(selectedButtons)
 
 function addButtons(selectedButtons, button)
 {
-  return selectedButtons => [...selectedButtons, button]
+  if ((button === '/' || button === '*') && (selectedButtons[selectedButtons.length - 1] === button))
+  {
+    return(selectedButtons)
+  }
+  else
+  {
+    return selectedButtons => [...selectedButtons, button]
+  }
+
 }
 
 function deleteLast(selectedButtons)
 {
-  return selectedButtons.splice(-1)
+  return selectedButtons.slice(0, -1)
 }
 
 export default App
