e6a344ad0cf98bb3536676ed3f64059a8e2c5731
Vlad <vladut_stoian@live.co.uk>
2022-08-23 14:23:58 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,75 +1,42 @@
-import React, { useState } from 'react';
-import './App.css';
-import { evaluate } from 'mathjs'
+import { React, useState } from 'react'
 
-const App = () => 
+
+
+const App = () =>
 {
-const calculatorButtons = [0,1,2,3,4,5,6,7,8,9,'+','-','/','*','C','Del','=']
-const [selectedButtons, setButtonSelection] = useState([])
-const [calculatorTotal, setCalculatorTotal] = useState([])
+
+  const [todos, setTodos] = useState([
+    { text: "Go shopping" },
+    { text: "Wash dishes" },
+    { text: "Study for the exam" }
+  ]);
+
+  const [userInput, setUserInput] = useState("")
 
   return (
-    <div className='calculatorContainer'>
-      <div className="calculatorTotalScreen">
-        <h1>Numbers: {selectedButtons}</h1>
-        <h1>Total: {calculatorTotal}</h1>
-      </div>
-      
-      <div className='calculatorButtons'>
-      {calculatorButtons.map((button, buttonKey) =>
-        {
-          if (button === '=') 
-          {
-            return(<button className='equalsButton' key= { buttonKey } onClick={() => setCalculatorTotal(returnResult(selectedButtons))}>=</button>)
-          }
-          else if (button === 'C')
-          {
-            return(<button key = { buttonKey } onClick={() => clearAll(setButtonSelection, setCalculatorTotal)}>C</button>)
-          }
-          else if (button === 'Del')
-          {
-            return(<button key = { buttonKey } onClick={() => setButtonSelection(deleteLast(selectedButtons))}>Del</button>)
-          }
-          else
-          {
-            return (<button key = { buttonKey } onClick={() => setButtonSelection(addButtons(selectedButtons, button))}>{button}</button>)
-          }
-        })}
+    <div className="app">
+      <div className="todo-app">
+        <ul className="current-todos">
+          {todos.map((todo, index) => (
+            <Todo key="index" todo={todo} />
+          ))}
+        </ul>
+        <form>
+          <input className="add-todo" type="text" value={userInput} onChange={textInput => setUserInput(textInput.target.value)} />
+          <p>{userInput}</p>
+        </form>
       </div>
     </div>
+  );
+};
+
+const Todo = (props) =>
+{
+  return (
+    <li>
+      {props.todo.text}
+    </li>
   )
-}
-
-function clearAll(setButtonSelection, setCalculatorTotal)
-{
-  return [setButtonSelection(""), setCalculatorTotal("")]
-}
-
-function returnResult(selectedButtons)  
-{
-  return evaluate(selectedButtons.join(""))
-}
-
-function addButtons(selectedButtons, button)
-{
-  if ((button === '/' || button === '*') && ((typeof selectedButtons[selectedButtons.length - 1] === 'string')))
-  {
-    return(selectedButtons)
-  }
-  else if ((button === '+' || button === '-') && (selectedButtons[selectedButtons.length - 1] === '/' || selectedButtons[selectedButtons.length - 1] === '*'))
-  {
-    return selectedButtons
-  }
-  else
-  {
-    return selectedButtons => [...selectedButtons, button]
-  }
-
-}
-
-function deleteLast(selectedButtons)
-{
-  return selectedButtons.slice(0, -1)
 }
 
 export default App
